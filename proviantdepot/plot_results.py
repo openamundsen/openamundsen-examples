ab2538fd23b4d80e9d35ad0306992d947e1d4dd1
Florian Hanzer <florian.hanzer@gmail.com>
2024-09-09 14:48:58 +0200

    Update proviantdepot/plot_results.py

--- proviantdepot/plot_results.py
+++ proviantdepot/plot_results.py
@@ -1,9 +1,14 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-depth_obs = pd.read_csv('obs_snow_depth.csv', parse_dates=True, index_col=0, squeeze=True)
+depth_obs = pd.read_csv('obs_snow_depth.csv', parse_dates=True, index_col=0).squeeze()
 df_sim = pd.read_csv('results/point_proviantdepot.csv', parse_dates=True, index_col=0)
-depth = pd.DataFrame(data=dict(obs=depth_obs, sim=df_sim.snow_depth))
+depth = pd.DataFrame(
+    data=dict(
+        obs=depth_obs.resample(df_sim.snow_depth.index.inferred_freq).mean(),
+        sim=df_sim.snow_depth,
+    ),
+)
 
 plt.close('all')
 depth.plot()
